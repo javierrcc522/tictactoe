f0aade29db74b5339b2919f8dcda9b44617cc5ba
javier jimenez, Keegan Ruebling and Riki Montgomery <javier@>
2017-08-24 16:07:35 -0700

    add beautiful tic tac toe functionality to our page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Back-End
 function Board () {
-  this.row1 = [0, 1, 2]
-  this.row2 = [0, 1, 2]
-  this.row3 = [0, 1, 2]
+  this.row1 = [0, 0, 0]
+  this.row2 = [0, 0, 0]
+  this.row3 = [0, 0, 0]
   this.matrix = [this.row1, this.row2, this.row3]
 };
 
@@ -16,10 +16,11 @@ var board = new Board ();
 var player1 = new Player ("Riki", "X");
 var player2 = new Player ("Javi", "O");
 
-function switch () {
-  if (board.matrix === this.mark("X")) {
+var turnTracker = 2
 
-  }
+Player.prototype.win = function() {
+  if (this.matrix[0][0] === "X")
+  alert ("hi");
 }
 
 //Front-End
@@ -28,41 +29,127 @@ $(document).ready(function() {
   //well [0][0]
   $('.well#1').click(function(event) {
     event.preventDefault();
-    board.matrix[0][0] = player1.mark
-    board.matrix[0][0] = player2.mark
-    console.log(board);
+    if (board.matrix[0][0] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[0][0] = player1.mark
+      $(".well#1 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[0][0] = player2.mark
+      $(".well#1 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [0][1]
   $('.well#2').click(function(event) {
-    board.matrix[0][1] = player1.mark
+    if (board.matrix[0][1] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[0][1] = player1.mark
+      $(".well#2 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[0][1] = player2.mark
+      $(".well#2 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [0][2]
   $('.well#3').click(function(event) {
+    if (board.matrix[0][2] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[0][2] = player1.mark
+      $(".well#3 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[0][2] = player2.mark
+      $(".well#3 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [1][0]
   $('.well#4').click(function(event) {
+    if (board.matrix[1][0] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[1][0] = player1.mark
+      $(".well#4 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[1][0] = player2.mark
+      $(".well#4 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [1][1]
   $('.well#5').click(function(event) {
+    if (board.matrix[1][1] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[1][1] = player1.mark
+      $(".well#5 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[1][1] = player2.mark
+      $(".well#5 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [1][2]
   $('.well#6').click(function(event) {
+    if (board.matrix[1][2] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[1][2] = player1.mark
+      $(".well#6 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[1][2] = player2.mark
+      $(".well#6 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [2][0]
   $('.well#7').click(function(event) {
+    if (board.matrix[2][0] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[2][0] = player1.mark
+      $(".well#7 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[2][0] = player2.mark
+      $(".well#7 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [2][1]
   $('.well#8').click(function(event) {
+    if (board.matrix[2][1] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[2][1] = player1.mark
+      $(".well#8 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[2][1] = player2.mark
+      $(".well#8 h3").text("O");
+      turnTracker += 1;
+    }
   });
 
   //well [2][2]
   $('.well#9').click(function(event) {
+    if (board.matrix[2][2] != 0) {
+    } else if (turnTracker % 2 === 0) {
+      board.matrix[2][2] = player1.mark
+      $(".well#9 h3").text("X");
+      turnTracker += 1;
+    } else {
+      board.matrix[2][2] = player2.mark
+      $(".well#9 h3").text("O");
+      turnTracker += 1;
+    }
   });
 });
